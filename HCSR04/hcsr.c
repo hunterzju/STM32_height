77fc1c99c78963d46ab69d30d5b8dd05617d8467
hunterzju <313821078@qq.com>
2016-05-18 23:53:20 +0800

    添加超声波文件
    仅添加了头文件和框架

---
+++ HCSR04/hcsr.c
@@ -0,0 +1,3 @@
+#include "hcsr.h"
+
+
---
+++ HCSR04/hcsr.h
@@ -0,0 +1,11 @@
+#ifndef HCSR_H
+#define HCSR_H
+
+#include "stm32f10x.h"
+#include "bsp_TiMbase.h"
+
+void Triger(void);						//∑¢ÀÕ≤‚¡ø–≈∫≈
+float Calculation(void);
+
+
+#endif
